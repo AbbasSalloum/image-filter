88db9863bc62077f29b5449861184280e4b2f0bb
AbbasSalloum <salloum916@gmail.com>
2021-10-06 10:01:26 -0400

    Second version of image filter

--- home.js
+++ home.js
@@ -7,7 +7,17 @@ function upload(){
   image.drawTo(imgcanvas);
 }
 
+function IsImageUploaded(){
+  if(image==null || !image.complete()){
+    alert("image not uploaded");
+    return false;
+  }
+  else{
+    return true;
+  }
+}
 function makeGray(){
+  if(IsImageUploaded(image)){
   for(var pixel of image.values()){
     var avg=((pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3)
     pixel.setRed(avg);
@@ -16,4 +26,55 @@ function makeGray(){
     }
   var imgcanvas=document.getElementById("can");
   image.drawTo(imgcanvas);
+}
+}
+
+function Reset(){
+  if(IsImageUploaded(image)){
+  var imgcanvas=document.getElementById("can");
+  var fileinput=document.getElementById("finput");
+  image=new SimpleImage(fileinput);
+  image.drawTo(imgcanvas);
+  }
+  
+}
+function makeRed(){
+  if(IsImageUploaded(image)){
+  for(var pixel of image.values()){
+    var avg=((pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3)
+    if(avg<128){
+    pixel.setRed(2*avg);
+    pixel.setGreen(0);
+    pixel.setBlue(0);
+    }
+    else{
+    pixel.setRed(255);
+    pixel.setGreen(2*avg -255);
+    pixel.setBlue(2*avg -255);
+    }
+    }
+  var imgcanvas=document.getElementById("can");
+  image.drawTo(imgcanvas);
+}
+}
+
+function makeBlue(){
+  if(IsImageUploaded(image)){
+  for(var pixel of image.values()){
+    var avg=((pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3)
+    if(avg<128){
+    pixel.setRed(0);
+    pixel.setGreen(0);
+    pixel.setBlue(2*avg);
+    }
+    else{
+    pixel.setRed(2*avg -255);
+    pixel.setGreen(2*avg -255);
+    pixel.setBlue(255);
+    }
+    }
+  var imgcanvas=document.getElementById("can");
+  image.drawTo(imgcanvas);
+}
+  
 }
